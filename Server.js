634c280509a5ad191d0975339b30ba025841f277
yousufabd11 <yousufabdi01@gmail.com>
2024-07-01 10:46:40 +0300

    first commit

---
+++ Server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+// Set EJS as the templating engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Middleware to check if the request is within working hours
+const workingHoursMiddleware = (req, res, next) => {
+  const date = new Date();
+  const day = date.getDay();
+  const hour = date.getHours();
+
+  if (day >= 1 && day <= 5 && hour >= 9 && hour < 17) {
+    next();
+  } else {
+    res.send('The web application is only available during working hours (Monday to Friday, from 9 to 17).');
+  }
+};
+
+// Apply the middleware to all routes
+app.use(workingHoursMiddleware);
+
+// Serve static files (CSS)
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+app.get('/services', (req, res) => {
+  res.render('services');
+});
+
+app.get('/contact', (req, res) => {
+  res.render('contact');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
